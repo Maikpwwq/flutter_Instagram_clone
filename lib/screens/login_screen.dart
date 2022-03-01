8b56fad491fd59f070402b86161bb56976cfc233
michael.arias <mariasf@mintic.gov.co>
2022-02-28 23:49:56 -0500

    responsive_media_query

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/screens/signup_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
 import 'package:instagram_flutter/utils/material.dart';
 import 'package:instagram_flutter/widgets/Text_field_input.dart';
 
@@ -39,12 +40,12 @@ class _LoginScreenState extends State<LoginScreen> {
     if (res == 'success') {
       // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const HomeScreen()));
       Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-              builder: (context) => const ResponsiveLayout(
-                mobileScreenLayout: MobileScreenLayout(),
-                webScreenLayout: WebScreenLayout(),
-              ),
+        MaterialPageRoute(
+          builder: (context) => const ResponsiveLayout(
+            mobileScreenLayout: MobileScreenLayout(),
+            webScreenLayout: WebScreenLayout(),
           ),
+        ),
       );
     } else {
       showSnackBar(res, context);
@@ -67,7 +68,9 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
+          padding: MediaQuery.of(context).size.width > webScreenSize
+              ? EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/3 )
+              : const EdgeInsets.symmetric(horizontal: 32),
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -9,6 +9,7 @@ import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
 import 'package:instagram_flutter/utils/material.dart';
 import 'package:instagram_flutter/widgets/Text_field_input.dart';
 
@@ -91,7 +92,9 @@ class _SignupScreenState extends State<SignupScreen> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
+          padding: MediaQuery.of(context).size.width > webScreenSize
+              ? EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/3 )
+              : const EdgeInsets.symmetric(horizontal: 32),
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
